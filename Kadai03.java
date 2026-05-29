d7c7f206522048a185338ecc6c355692f356014a
kanoe <bakoutaku@gmail.com>
2026-05-29 17:30:43 +0900

    Update and rename to Kadai03.java

--- Kadai03.java
+++ Kadai03.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Kadai03_PI11A_25 
+public class Kadai03 
 {
 	public static void main(String[] args) throws IOException
 	{
